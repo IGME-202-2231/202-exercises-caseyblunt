c4a7c476b0f0d92912756783f93cf151f3ab0252
caseyblunt <112418028+caseyblunt@users.noreply.github.com>
2023-09-18 13:43:34 -0400

    finished e03

--- Exercise 3/Assets/Scripts/ClockNumbers.cs
+++ Exercise 3/Assets/Scripts/ClockNumbers.cs
@@ -5,10 +5,31 @@ using UnityEngine;
 public class ClockNumbers : MonoBehaviour
 {
     public GameObject number;
+
+    float angle;
+    float radians;
+    float radius;
+
+    Vector3 position;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 1; i <= 12; i++)
+        {
+            angle = i * 360f / 12f - 120;
+            radians = angle * Mathf.Deg2Rad;
+
+            radius = 2f;
+            position = new Vector3(radius * Mathf.Cos(radians), -radius * Mathf.Sin(radians), 0);
+
+            Instantiate(number, position, Quaternion.identity);
+
+            TextMesh textMesh = number.GetComponent<TextMesh>();
+            if (textMesh != null)
+            {
+                textMesh.text = i.ToString();
+            }
+        }
     }
 
     // Update is called once per frame
