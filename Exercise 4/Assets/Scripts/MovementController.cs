901813ecee1d389c48e0ae69c51c6ad64df2eaee
caseyblunt <112418028+caseyblunt@users.noreply.github.com>
2023-09-25 19:32:41 -0400

    finished e04

--- Exercise 4/Assets/Scripts/MovementController.cs
+++ Exercise 4/Assets/Scripts/MovementController.cs
@@ -8,9 +8,20 @@ public class MovementController : MonoBehaviour
     Vector3 direction = Vector3.right;
     Vector3 velocity = Vector3.zero;
     [SerializeField] float speed = 1.0f;
+    float minX, maxX, minY, maxY;
+
     void Start()
     {
         objectPos = transform.position;
+
+        // Calculate screen boundaries
+        Camera mainCamera = Camera.main;
+        float objectWidth = GetComponent<Renderer>().bounds.size.x;
+        float objectHeight = GetComponent<Renderer>().bounds.size.y;
+        minX = mainCamera.ViewportToWorldPoint(Vector3.zero).x + objectWidth / 2;
+        maxX = mainCamera.ViewportToWorldPoint(Vector3.one).x - objectWidth / 2;
+        minY = mainCamera.ViewportToWorldPoint(Vector3.zero).y + objectHeight / 2;
+        maxY = mainCamera.ViewportToWorldPoint(Vector3.one).y - objectHeight / 2;
     }
 
     // Update is called once per frame
@@ -20,7 +31,13 @@ public class MovementController : MonoBehaviour
 
         objectPos += velocity;
 
-        //Check position
+        // Wrap around when reaching the screen borders
+        if (objectPos.x < minX) objectPos.x = maxX;
+        if (objectPos.x > maxX) objectPos.x = minX;
+        if (objectPos.y < minY) objectPos.y = maxY;
+        if (objectPos.y > maxY) objectPos.y = minY;
+
+        // Update position
         transform.position = objectPos;
     }
 
@@ -33,7 +50,7 @@ public class MovementController : MonoBehaviour
             if (direction != Vector3.zero)
             {
                 transform.rotation = Quaternion.LookRotation(Vector3.back, direction);
-            }           
-        } 
+            }
+        }
     }
 }
